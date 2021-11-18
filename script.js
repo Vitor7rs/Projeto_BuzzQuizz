8cecf8eec72f2b505cd00b3ce5b61081a4fd68d4
C137Rodrigolima <rodrigolimac137@gmail.com>
2021-11-18 00:40:39 -0300

    Desktop1 quase pronto

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-/** criando java*/
+const PromessaTodosQuizzes = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+PromessaTodosQuizzes.then(TratarSucesso);
+
+function TratarSucesso(resposta) {
+    console.log(resposta.data);
+    ColocarTodosQuizzes(resposta.data);
+}
+
+function ColocarTodosQuizzes(Objetopai){
+    const ComprimentoObjetosQuizzees = Object.keys(Objetopai).length;
+    //acima tem o LENGTH do Objeto com o NUMERO TOTAL DE QUIZZES!!!
+
+    const BlocodeQuizzes = document.querySelector(".Todos-Quizz");
+
+    for(let i=0; i<ComprimentoObjetosQuizzees; i++){
+        BlocodeQuizzes.innerHTML += `
+        <div class="bloco-cada-quizz"><img class="img-cada-quizz" src=${Objetopai[i].image}></div><div class="titulo-cada-quizz">${Objetopai[i].title}</div>`;
+    }
+}
